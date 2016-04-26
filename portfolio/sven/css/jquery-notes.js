6fd33ab79339e911d0ecf031964a454a9f4f172d
Sven van Hoorn <svenvanhoorn@gmail.com>
2016-04-26 17:33:42 +0200

    contact section upgrade, social icons added and onclick, open in new tab

---
+++ portfolio/sven/css/jquery-notes.js
@@ -0,0 +1,17 @@
+
+Jquery
+	write less and do more with javascript. It makes it easier to program
+
+Use version 2.X version
+JQuery is not supported by IE 6, 7, 8
+
+Jquery ccan be loaded from CDN or file
+
+first load library after it load the code
+
+when working on a website:
+know what is included and what it does
+
+
+callBack function:
+	
